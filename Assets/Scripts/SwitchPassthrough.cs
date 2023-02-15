a7678a6182da74a805bce580cd1890b33930146d
Lina Ota <otlina619421@gmail.com>
2023-02-15 19:59:48 +0900

    Add: Meta Avatar
    アバターは導入成功、パススルーがうまくいかない

---
+++ Assets/Scripts/SwitchPassthrough.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPassthrough : MonoBehaviour
+{
+    OVRPassthroughLayer passthroughLayer;
+    [SerializeField]
+    OVRCameraRig ovrCameraRig;
+    [SerializeField]
+    GameObject avatar1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //var ovrCameraRig = GameObject.FindObjectOfType<OVRCameraRig>();
+        if (ovrCameraRig == null)
+        {
+            Debug.LogError("Scene does not contain an OVRCameraRig");
+            return;
+        }
+
+        passthroughLayer = ovrCameraRig.GetComponent<OVRPassthroughLayer>();
+        if (passthroughLayer == null)
+        {
+            Debug.LogError("OVRCameraRig does not contain an OVRPassthroughLayer component");
+        }
+    }
+
+    private void Update()
+    {
+        if(OVRInput.Get(OVRInput.Button.One))
+        {
+            avatar1.SetActive(false);
+            Passthrough();
+        }
+    }
+
+    private void Passthrough()
+    {
+        passthroughLayer.hidden = !passthroughLayer.hidden;
+    }
+}
